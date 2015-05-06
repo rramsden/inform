efe4399ed2ac99210790af9ef380fc7e9305d162
Richard Ramsden <richard@rramsden.ca>
2015-05-06 17:00:02 +0900

    Add base layout for inform

--- Foundation.hs
+++ Foundation.hs
@@ -61,6 +61,7 @@ instance Yesod App where
 
         pc <- widgetToPageContent $ do
             addStylesheet $ StaticR css_bootstrap_css
+            addStylesheet $ StaticR css_screen_css
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
