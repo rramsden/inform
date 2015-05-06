b966ef6bce72694b239f29f19bfd86541d5ce4b2
Richard Ramsden <richard@rramsden.ca>
2015-05-06 15:21:49 +0900

    Add link details

---
+++ Handler/LinkDetails.hs
@@ -0,0 +1,6 @@
+module Handler.LinkDetails where
+
+import Import
+
+getLinkDetailsR :: LinkId -> Handler Html
+getLinkDetailsR linkId = error "Not yet implemented: getLinkDetailsR"
---
+++ Handler/LinkNew.hs
@@ -0,0 +1,26 @@
+module Handler.LinkNew where
+
+import Import
+
+-- data InputForm = InputForm
+--           { email :: Text
+--           }
+
+newLinkForm :: AForm Handler Link
+newLinkForm = Link
+  <$> areq textField "contact_email" Nothing
+
+getLinkNewR :: Handler Html
+getLinkNewR = do
+        (widget, enctype) <- generateFormPost $ renderDivs newLinkForm
+        defaultLayout $ do
+            $(widgetFile "forms/new")
+
+postLinkNewR :: Handler Html
+postLinkNewR = do
+        ((res, widget), enctype) <- runFormPost $ renderDivs newLinkForm
+        case res of
+            FormSuccess link -> do
+                _ <- runDB $ insert link
+                error "todo"
+            _ -> defaultLayout $(widgetFile "forms/new")
